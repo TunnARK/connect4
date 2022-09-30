918b6e9f91f6c95e2ce552ee99c100038ba82e4e
TunnARK <96030186+TunnARK@users.noreply.github.com>
2022-09-30 16:08:19 +0200

    Q1: completed

--- connect4/dumb_ia.py
+++ connect4/dumb_ia.py
@@ -1,6 +1,14 @@
-from .game import Grid, Player
+from re import A
+from .game import Cell, Game, Grid, Player
 
 
 class DumbIA(Player):
+    # Doit retourner la column a jouer (ici la premiere colonne libre)
     def play(self, grid: Grid) -> int:
-        ...
+        # Parcours des colonnes pour trouver la premiere colonne libre
+        for line in range(grid.lines): 
+            for column in range(grid.columns):
+            # test si cell empty
+                if grid.grid[line][column] == Cell.EMPTY:
+                    return column
+        raise ValueError(f"Grid is full")
