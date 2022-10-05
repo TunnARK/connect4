ffc5b31552812873ba7f5c55ccc7941e3227126e
TunnARK <96030186+TunnARK@users.noreply.github.com>
2022-10-05 10:52:02 +0200

    Q5: completed

--- connect4/cheater_b.py
+++ connect4/cheater_b.py
@@ -1,8 +1,15 @@
-from .game import Grid, Player
+from .game import Game, Grid, Player, Cell
+from connect4 import game
 
 
 class CheaterB(Player):
     """This IA cheats and modify the grid to ensure player B wins."""
 
     def play(self, grid: Grid) -> int:
-        ...
+        #print(grid)
+        cell = Cell.B
+        Grid.place(grid, 1, cell)
+        Grid.place(grid, 2, cell)
+        Grid.place(grid, 3, cell)
+        #print(grid)
+        return 4
