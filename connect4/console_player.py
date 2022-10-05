c4552095f45969bc058a08a3ee368194ba42b14f
TunnARK <96030186+TunnARK@users.noreply.github.com>
2022-10-05 10:18:54 +0200

    Q4: completed

--- connect4/console_player.py
+++ connect4/console_player.py
@@ -1,9 +1,10 @@
 from .game import Grid, Player
 
-
 class ConsolePlayer(Player):
     """Allow a human to see the grid in its shell, and input a column from the
     keyboard."""
 
     def play(self, grid: Grid) -> int:
-        ...
+        print(grid)
+        col = int(input("Input Column Number:"))
+        return col
